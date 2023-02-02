ce22b17715197a343e8df69e316a038345058f57
ilhamarifinali <fansghoib7@gmail.com>
2023-02-02 09:21:41 +0700

    Initial commit

---
+++ resources/views/siswa.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman siswa</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Sekolah</a>
+        <a class="nav-link" href="#">Beranda</a>
+        <a class="nav-link" href="#">Siswa</a>
+        <a class="nav-link disabled">Tentang</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    <h1>
+     number
+    </h1>
+</body>
+</html>
